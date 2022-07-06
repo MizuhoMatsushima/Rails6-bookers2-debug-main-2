9b46fc9897d2a3b3ec1a58faafb0ae6bae77b87b
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-07-06 06:38:58 +0000

    [Update]Rails6-bookers2-debug-main_2

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,11 @@ class Book < ApplicationRecord
   #PV数の計測
   is_impressionable
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: (Time.current - 6.day).at_beginning_of_day...(Time.current).at_end_of_day) }  # 今週
+  scope :created_last_week, -> { where(created_at: (Time.current - 13.day).at_beginning_of_day...(Time.current - 7.day).at_end_of_day) } # 前週
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
