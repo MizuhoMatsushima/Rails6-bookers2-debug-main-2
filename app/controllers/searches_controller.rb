46a62b8e4dd8303cc60ade167bfb3ea46273c53b
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-07-07 10:13:15 +0000

    [Update]Rails6-bookers2-debug-main_2

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,7 +9,6 @@ before_action :authenticate_user!
 
   def search
     @range = params[:range]
-
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
       render :index
